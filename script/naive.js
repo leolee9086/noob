a23a0e1fe8b8eb537c9b8e9b5ceee50072a865f8
leolee9086 <464803712@qq.com>
2022-06-10 14:41:44 +0800

    分离nodered到插件

--- script/naive.js
+++ script/naive.js
@@ -30,6 +30,7 @@ export default class naive {
     };
     this.corePlugins = {};
     this.publishPlugins = {}
+    this.plugins=[]
     this.初始化核心插件();
     this.plugin = 主题插件;
     window.addEventListener("keyup", (event) => this.判断键盘目标(event));
@@ -198,7 +199,6 @@ export default class naive {
       return true
     }
   }
-
   bindURL(url, baseURL) {
     console.log(baseURL);
     let realURL = new URL(url, baseURL);
@@ -211,5 +211,53 @@ export default class naive {
         this.加载核心插件(插件名, 插件环境配置[环境]);
       }
     }
+  }
+  生成默认设置(customoption, workspaceDir, userId) {
+    let 思源伺服端口 = 6806;
+    let 思源伺服地址 = "127.0.0.1";
+    let option = {
+      发布地址: 思源伺服地址,
+      思源伺服地址: 思源伺服地址,
+      思源伺服端口: 思源伺服端口,
+      基础样式: `http://${customoption.发布地址 || 思源伺服地址}:${
+        customoption.发布端口 || 思源伺服端口
+      }/stage/build/export/base.css`,
+      发布主题: `http://${customoption.发布地址 || 思源伺服地址}:${
+        customoption.发布端口 || 思源伺服端口
+      }/appearance/themes/${
+        window.siyuan.config.appearance.themeDark
+      }/theme.css`,
+      发布脚本: `path:${workspaceDir}\\conf\\appearance\\themes\\naive\\config\\naive.js`,
+      高亮样式: `http://${customoption.发布地址 || 思源伺服地址}:${
+        customoption.发布端口 || 思源伺服端口
+      }/stage/protyle/js/highlight.js/styles/github.min.css`,
+      空页面内容: `path:${workspaceDir}\\conf\\appearance\\themes\\naive\\config\\naive.html`,
+      首页: {
+        思源文档id: "20200812220555-lj3enxa",
+      },
+      有限分享: false,
+      即时分享: true,
+      使用图床资源: true,
+      发布端口: 80,
+      思源账号id: userId,
+      发布图标: "",
+      暴露api: false,
+      暴露挂件: false,
+      暴露附件: false,
+      脚注内容: `path:${workspaceDir}\\conf\\appearance\\themes\\naive\\script\\footer.html`,
+      单块分享: true,
+      允许搜索: false,
+    };
+    option.workspace = workspaceDir;
+    for (let prop in option) {
+      customoption[prop] !== ""
+        ? (option[prop] = customoption[prop])
+        : (option[prop] = option[prop]);
+    }
+    if (option.首页 && !option.首页.思源文档id) {
+      option.首页.思源文档id = "20200812220555-lj3enxa";
+    }
+    option.workspace = workspaceDir;
+    return JSON.parse(JSON.stringify(option));
   }
 }
