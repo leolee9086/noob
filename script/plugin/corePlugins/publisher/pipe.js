dcf5680effce4d488a0b284c9c8511be1c0dc60c
leolee9086 <464803712@qq.com>
2022-09-13 21:49:06 +0800

    整合发布插件

--- script/plugin/corePlugins/publisher/pipe.js
+++ script/plugin/corePlugins/publisher/pipe.js
@@ -2,8 +2,7 @@ import {注入块属性} from "./renders/attributes.js"
 import {生成发布文档头} from "./renders/headers.js"
 import {生成发布文档内容} from "./renders/content.js"
 import {生成文档标题} from "./renders/title.js"
-import { 修改嵌入块 } from "./renders/modifiers.js"
-import { 修改块链接 } from "./修改块链接"
+import { 修改嵌入块,修改块链接,修改私有块 } from "./renders/modifiers.js"
 export const pipe = [
   注入块属性,
   生成发布文档头,
@@ -16,21 +15,3 @@ export const pipe = [
 
 
 
-async function 修改私有块(req, res, 渲染结果) {
-  if (!naive.私有块字典) {
-    naive.私有块字典 = {}
-  }
-
-  let 私有块数组 = 渲染结果.querySelectorAll('[custom-publish-token]')
-  私有块数组.forEach(
-    私有块 => {
-      if (!naive.私有块字典) {
-        naive.私有块字典 = {}
-      }
-      naive.私有块字典[私有块.getAttribute('data-node-id')] = { content: 私有块.innerHTML, token: 私有块.getAttribute("custom-publish-token") }
-      私有块.innerHTML = `<div>请输入块token<input/><button data-type="customAuthToken" data-node-id='${私有块.getAttribute('data-node-id')}'>确认</button></div>`
-      私有块.setAttribute("custom-publish-token", '')
-    }
-  )
-  return 渲染结果
-}
--- script/plugin/corePlugins/publisher/renders/modifiers.js
+++ script/plugin/corePlugins/publisher/renders/modifiers.js
@@ -57,3 +57,85 @@ export async function 获取嵌入块内容(嵌入块, docid) {
   嵌入块.innerHTML = 嵌入块内容 + 嵌入块.innerHTML;
   return 嵌入块.innerHTML;
 }
+export async function 修改私有块(req, res, 渲染结果) {
+  if (!naive.私有块字典) {
+    naive.私有块字典 = {}
+  }
+
+  let 私有块数组 = 渲染结果.querySelectorAll('[custom-publish-token]')
+  私有块数组.forEach(
+    私有块 => {
+      if (!naive.私有块字典) {
+        naive.私有块字典 = {}
+      }
+      naive.私有块字典[私有块.getAttribute('data-node-id')] = { content: 私有块.innerHTML, token: 私有块.getAttribute("custom-publish-token") }
+      私有块.innerHTML = `<div>请输入块token<input/><button data-type="customAuthToken" data-node-id='${私有块.getAttribute('data-node-id')}'>确认</button></div>`
+      私有块.setAttribute("custom-publish-token", '')
+    }
+  )
+  return 渲染结果
+}
+export function 修改块链接(req, res, div) {
+  let links = div.querySelectorAll("a");
+  if (links[0]) {
+    links.forEach((a) => {
+      let href = a.getAttribute("href");
+      a.setAttribute("href", href.replace("siyuan://blocks/", `/block/`));
+      href.indexOf("siyuan://") == 0
+        ? a.setAttribute("type", "blockref")
+        : null;
+    });
+  }
+  let blockrefs = div.querySelectorAll('span[data-type="block-ref"]');
+  if (blockrefs[0]) {
+    blockrefs.forEach((a) => {
+      let link = document.createElement("a");
+      link.innerHTML = a.innerHTML;
+      link.setAttribute("data-type", a.getAttribute("data-type"));
+      link.setAttribute("data-id", a.getAttribute("data-id"));
+      link.setAttribute("type", "blockref");
+      link.setAttribute("href", `/block/${a.getAttribute("data-id")}`);
+      a.innerHTML = '';
+      a.appendChild(link);
+
+    });
+  }
+  let contentdivs = div.querySelectorAll('*[contenteditable="true"]');
+  if (contentdivs[0]) {
+    contentdivs.forEach((contentdiv) => {
+      contentdiv.setAttribute("contenteditable", "false");
+    });
+  }
+  let outlinks = div.querySelectorAll('span[data-type="a"]');
+  if (outlinks[0]) {
+    outlinks.forEach((a) => {
+      let link = document.createElement("a");
+      link.innerHTML = a.innerHTML;
+      link.setAttribute("data-type", a.getAttribute("data-type"));
+      link.setAttribute("data-id", a.getAttribute("data-id"));
+      link.setAttribute("type", "a");
+      link.setAttribute("href", a.getAttribute("data-href"));
+      a.innerHTML = '';
+      a.appendChild(link);
+
+    });
+  }
+  let assets = div.querySelectorAll("a[src]");
+  if (assets[0]) {
+    assets.forEach((a) => {
+
+      let src = a.getAttribute("src");
+      if (src.indexOf("assets") == 0) {
+        if (naive.设置.使用图床资源) {
+          a.setAttribute(
+            "src",
+            naive.设置.发布图床前缀 + src.slice(7, src.length)
+          );
+        } else {
+          a.setAttribute("src", `/` + src);
+        }
+      }
+    });
+  }
+  return div;
+}
