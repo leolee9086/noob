2839c4bb40e193a391af6a8e3a103d0cf78918c0
leolee9086 <464803712@qq.com>
2022-09-20 12:25:27 +0800

    整理代码

--- script/server/routers/index.js
+++ script/server/routers/index.js
@@ -5,33 +5,26 @@ router.use('/naiveApi/', require("./naiveApi/index.js"))
 router.use("/user/", require("./user/index.js"))
 router.use("/unauthorized/", require("./unauthorized/index.js"))
 naive.pluginsApiRouter = require("./pluginsApi/index.js")
-
-try {
-    router.use("/siyuanApi/", require("./siyuanApi/index.js"))
-    router.use("/api/", require("./siyuanApi/index.js"))
-
-    const { createProxyMiddleware } = require('http-proxy-middleware')
-    const {
-        debugProxyErrorsPlugin, // subscribe to proxy errors to prevent server from crashing
-        loggerPlugin, // log proxy events to a logger (ie. console)
-        errorResponsePlugin, // return 5xx response on proxy error
-        proxyEventsPlugin, // implements the "on:" option
-      } = require('http-proxy-middleware');
-    const proxy = createProxyMiddleware({
-        target: `ws://${naive.publishOption.思源伺服地址}:${naive.publishOption.思源伺服端口}/ws`,
-        changeOrigin: false,
-        router:function(req){
-            console.log(req.url)
-            return `ws://${naive.publishOption.思源伺服地址}:${naive.publishOption.思源伺服端口}${req.url}`
-        },
-        ws: true,
-        logLevel : 'debug'
-    }
-    )
-    naive.wsProxy = proxy
-    router.use("/ws",proxy)
-
-} catch (e) {
-    console.error(e)
+router.use("/siyuanApi/", require("./siyuanApi/index.js"))
+router.use("/api/", require("./siyuanApi/index.js"))
+const { createProxyMiddleware } = require('http-proxy-middleware')
+const {
+    debugProxyErrorsPlugin, // subscribe to proxy errors to prevent server from crashing
+    loggerPlugin, // log proxy events to a logger (ie. console)
+    errorResponsePlugin, // return 5xx response on proxy error
+    proxyEventsPlugin, // implements the "on:" option
+} = require('http-proxy-middleware');
+const proxy = createProxyMiddleware({
+    target: `ws://${naive.publishOption.思源伺服地址}:${naive.publishOption.思源伺服端口}/ws`,
+    changeOrigin: false,
+    router: function (req) {
+        console.log(req.url)
+        return `ws://${naive.publishOption.思源伺服地址}:${naive.publishOption.思源伺服端口}${req.url}`
+    },
+    ws: true,
+    logLevel: 'debug'
 }
+)
+naive.wsProxy = proxy
+router.use("/ws", proxy)
 module.exports = router
