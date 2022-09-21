c2904a94a9a9064212b4ed91de708edf5fbaaed0
leolee9086 <464803712@qq.com>
2022-09-21 19:50:19 +0800

    重构设置

--- script/server/middleWares/auth.js
+++ script/server/middleWares/auth.js
@@ -78,44 +78,9 @@ naive.syAuthConfig = {
     api: {
         insertBlock: {
             access: { user_group: 'admin' },
-            preFix: function (data, rawReq, rawRes) {
-                console.log(syReq)
-            },
-            afterFix: async function (data, rawReq, rawRes) {
-                console.log(data)
-            }
         },
         transactions: {
             access: { user_group: 'admin' },
-            preFix:async function (data, rawReq, rawRes) {
-                console.log(data)
-                if(data.transactions){
-                    data.transactions.forEach(
-                        transaction=>{
-                            transaction.doOperations.forEach(
-                                Operation=>{
-                                    Operation.data=Operation.data.replace(/\&lt\;span\&gt\;/g,'<span>')
-                                    Operation.data=Operation.data.replace(/\&lt\;\/span&gt\;/g,'</span>')
-
-                                    naive.核心api.setBlockAttrs({
-                                        id:Operation.id,
-                                        attrs:{
-                                            'custom-lastedit-ip':rawReq.socket.remoteAddress,
-                                            'custom-lastedit-user':rawReq.session.user||'visitor',
-                                            'custom-lastedit-describe':`来自${rawReq.socket.remoteAddress}的${rawReq.session.user||'visitor'} 编辑于${new Date().toString()} `,                                            
-                                        }
-                                    },'')
-                                }
-                            )
-                        }
-                    )
-                }
-                return data
-            },
-            afterFix: async function (data, rawReq, rawRes) {
-                console.log(data)
-                return data
-            }
         },
         prependBlock: { user_group: 'admin' },
         appendBlock: { user_group: 'admin' },
@@ -124,15 +89,6 @@ naive.syAuthConfig = {
         setBlockReminder: { user_group: 'admin' },
         setBlockAttrs: {             
             access: { user_group: 'admin' },
-        preFix: function (syReq, rawReq, rawRes) {
-            console.log(syReq)
-        },
-        afterFix: async function (syRes, rawReq, rawRes) {
-            console.log(syRes)
-            let str= await unzip(syRes)
-            console.log(str)
-            rawRes.end(JSON.stringify(str))
-        }
 },
         upload: { user_group: 'admin' }
     }
