cca90d378da500a9c3a294f0d304254925db68c9
leolee9086 <464803712@qq.com>
2022-09-22 21:50:53 +0800

    删除无用代码

--- script/server/routers/index.js
+++ script/server/routers/index.js
@@ -7,15 +7,8 @@ router.use("/user/", require("./user/index.js"))
 router.use("/unauthorized/", require("./unauthorized/index.js"))
 naive.pluginsApiRouter = require("./pluginsApi/index.js")
 router.use("/api/", require("./siyuanApi/index.js"))
-const {discribeApi} = naive.serverUtil
 //这里用于对websocket进行转发
 const { createProxyMiddleware } = require('http-proxy-middleware')
-const {
-    debugProxyErrorsPlugin, // subscribe to proxy errors to prevent server from crashing
-    loggerPlugin, // log proxy events to a logger (ie. console)
-    errorResponsePlugin, // return 5xx response on proxy error
-    proxyEventsPlugin, // implements the "on:" option
-} = require('http-proxy-middleware');
 const proxy = createProxyMiddleware({
     target: `ws://${naive.publishOption.思源伺服地址}:${naive.publishOption.思源伺服端口}/ws`,
     changeOrigin: false,
