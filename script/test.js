bb2b40f74b4562ea6cdc039cbc4fab6c61691e74
leolee9086 <464803712@qq.com>
2023-03-05 14:40:47 +0800

    恢复插件系统

--- script/test.js
+++
@@ -1,9 +0,0 @@
-import { getAllModels, getAllTabs } from "./noobApi/util/layout.js"
-import commonMenu from "./noobApi/registries/commonMenu.js"
-document.addEventListener(
-    "click", (event) => {
-        console.log(getAllModels())
-        console.log(getAllTabs())
-    }, true
-)
-
